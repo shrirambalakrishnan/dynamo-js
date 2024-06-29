a940f378a9aec2ca9dc7cdb4d0b3b8d1805ff74c
shriramrbk <shriramrbk@gmail.com>
2024-06-29 20:44:28 +0530

    Add main function to simulate data storage across distributed servers

---
+++ index.js
@@ -0,0 +1,66 @@
+const { ConsistentHashing } = require("./consistenHashing");
+const { ServerNode } = require("./serverNode");
+
+function initServers() {
+  let serverNode1 = new ServerNode({ id: 1, name: "server1", ip: "server1-ip" })
+  let serverNode2 = new ServerNode({ id: 2, name: "server2", ip: "server2-ip" })
+  let serverNode3 = new ServerNode({ id: 3, name: "server3", ip: "server3-ip" })
+  let serverNode4 = new ServerNode({ id: 4, name: "server4", ip: "server4-ip" })
+
+  let servers = [
+    serverNode1,
+    serverNode2,
+    serverNode3,
+    serverNode4,
+  ]
+
+  return servers
+}
+
+function seedData() {
+  let dataToStore = [
+    { key: "A001", value: {name: "user1", id: 1, address: "user1-address"} },
+    { key: "A002", value: {name: "user2", id: 2, address: "user2-address"} },
+    { key: "B003", value: {name: "user3", id: 3, address: "user3-address"} },
+    { key: "C004", value: {name: "user4", id: 4, address: "user4-address"} },
+    { key: "D005", value: {name: "user5", id: 5, address: "user5-address"} },
+    { key: "0006", value: {name: "user6", id: 6, address: "user6-address"} },
+  ]
+
+  return dataToStore
+}
+
+function loadData(servers, consistenHashing, dataToStore) {
+
+  dataToStore.forEach( data => {
+    const serverId = consistenHashing.getServer(data.key)
+    const server = servers.find( server => server.id == serverId )
+    server.put(data.key, data.value)
+  })
+  
+}
+
+function printServersData(servers) {
+  servers.forEach( server => {
+    console.log("-----------------------------------")
+    console.log("server = ", server.id)
+    console.log("data stored = ", JSON.stringify(server.data))
+  })
+}
+
+function startSimulation() {
+  let servers = initServers()
+  let dataToStore = seedData()
+
+  let consistenHashing = new ConsistentHashing( servers )
+  consistenHashing.constructRing()
+
+  loadData(servers, consistenHashing, dataToStore)
+  printServersData(servers)
+}
+
+function main() {
+  startSimulation()
+}
+
+main()
